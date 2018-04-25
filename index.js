061df38c2a31be9e804453c3e19911bcfc0930d0
Osman Mohamed <omohamed7890@gmail.com>
2018-04-25 23:34:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
+<<<<<<< HEAD
 
+=======
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+>>>>>>> 1a859715472ec524dd63d1d4e464e525c2fa1929
 // Don't worry about this
 module.exports = { name, height
 }
